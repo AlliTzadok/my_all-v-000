67bc5cb08b8751e9b48b4d3aad76df54e074c881
Alli Tzadok <allisontzadok@gmail.com>
2018-01-09 18:40:00 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def my_all?(collection)
   i = 0
   block_return_values = []
@@ -8,4 +6,10 @@ def my_all?(collection)
     i += 1
   end
 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
+
